4ea4dcb815af517611e0f99b15b77168529f1f94
Matt Kelsey <kelseym@mir.wustl.edu>
2018-05-22 15:55:17 -0500

    Start of imageassessor event trigger to detect ROI assessor w/RTSTRUCT resource.

---
+++ src/main/java/org/nrg/xnat/eventservice/events/ImageAssessorSaveEvent.java
@@ -0,0 +1,43 @@
+package org.nrg.xnat.eventservice.events;
+
+import org.nrg.framework.event.XnatEventServiceEvent;
+import org.nrg.xdat.model.XnatImageassessordataI;
+import org.nrg.xnat.eventservice.listeners.EventServiceListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@XnatEventServiceEvent(name="ImageAssessorSaveEvent")
+public class ImageAssessorSaveEvent extends CombinedEventServiceEvent<SessionUpdateEvent, XnatImageassessordataI> {
+
+    public ImageAssessorSaveEvent(){};
+
+    public ImageAssessorSaveEvent(XnatImageassessordataI payload, String eventUser) {
+        super(payload, eventUser);
+    }
+
+
+    @Override
+    public String getDisplayName() {
+        return "Image Assessor Saved";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Image Assessor Saved";
+    }
+
+    @Override
+    public String getPayloadXnatType() {
+        return "xnat:";
+    }
+
+    @Override
+    public Boolean isPayloadXsiType() {
+        return true;
+    }
+
+    @Override
+    public EventServiceListener getInstance() {
+        return new ImageAssessorSaveEvent();
+    }
+}
